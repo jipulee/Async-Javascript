210640a8fa09ad9fc413264c217230e0d663110b
khalil <jishen027@gmail.com>
2020-09-17 13:56:53 +0800

    async XMLHttprequest

---
+++ src/sandbox.js
@@ -0,0 +1,23 @@
+const getTodos = (callback) => {
+  const request = new XMLHttpRequest()
+
+
+  request.addEventListener('readystatechange', () => {
+    // console.log(request, request.readyState)
+    if (request.readyState === 4 && request.status == 200) {
+      callback(undefined, request.responseText)
+    } else if (request.readyState === 4) {
+      callback('could not fetch the data', undefined)
+    }
+  })
+  request.open('GET', 'https://jsonplaceholder.typicode.com/todos')
+  request.send()
+}
+
+getTodos((err, data) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(data)
+  }
+});
