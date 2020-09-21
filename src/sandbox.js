785fcd6465cde7fcaa1ed18750ccb989627b1992
khalil <jishen027@gmail.com>
2020-09-21 17:37:54 +0800

    async event listener

--- src/sandbox.js
+++ src/sandbox.js
@@ -5,7 +5,9 @@ const getTodos = (callback) => {
   request.addEventListener('readystatechange', () => {
     // console.log(request, request.readyState)
     if (request.readyState === 4 && request.status == 200) {
-      callback(undefined, request.responseText)
+      const data = JSON.parse(request.responseText)
+      callback(undefined, data)
+
     } else if (request.readyState === 4) {
       callback('could not fetch the data', undefined)
     }
